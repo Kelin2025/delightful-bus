fb9221ee0fd614e3f0ac8b7056c6e1fc01d0d13d
Anton Kosykh <kelin2025@yandex.ru>
2018-04-28 18:53:13 +0300

    Moved eventsList outside scope (to save scope when using in another objects)

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ export const send = events => to => {
   return to
 }
 
-export default function Eventbus(eventsList, instance) {
-  this.events = (eventsList && Object.assign({}, eventsList)) || {}
+export default function Eventbus(eventsList) {
+  eventsList = (eventsList && Object.assign({}, eventsList)) || {}
 
-  instance = instance || this
+  this.events = eventsList
 
   /**
    * Adds event listener
@@ -17,12 +17,12 @@ export default function Eventbus(eventsList, instance) {
    * @returns Emitter instance or instance with binded emitter
    */
   this.on = (evt, cb) => {
-    if (evt in this.events) {
-      this.events[evt] = this.events[evt].concat(cb)
+    if (evt in eventsList) {
+      eventsList[evt] = eventsList[evt].concat(cb)
     } else {
-      this.events[evt] = [cb]
+      eventsList[evt] = [cb]
     }
-    return instance
+    return this
   }
 
   /**
@@ -32,10 +32,10 @@ export default function Eventbus(eventsList, instance) {
    * @returns Emitter instance or instance with binded emitter
    */
   this.off = (evt, cb) => {
-    if (!(evt in this.events)) return instance
-    const idx = this.events[evt].indexOf(cb)
-    if (idx !== -1) this.events[evt].splice(idx, 1)
-    return instance
+    if (!(evt in eventsList)) return this
+    const idx = eventsList[evt].indexOf(cb)
+    if (idx !== -1) eventsList[evt].splice(idx, 1)
+    return this
   }
 
   /**
@@ -43,8 +43,8 @@ export default function Eventbus(eventsList, instance) {
    * @returns Emitter instance or instance with binded emitter
    */
   this.offAll = () => {
-    this.events = {}
-    return instance
+    eventsList = {}
+    return this
   }
 
   /**
@@ -54,8 +54,8 @@ export default function Eventbus(eventsList, instance) {
    * @returns {void}
    */
   this.emit = function(evt) {
-    if (!(evt in this.events)) return
-    this.events[evt].forEach(cb => cb([].slice.call(arguments, 1)))
+    if (!(evt in eventsList)) return
+    eventsList[evt].forEach(cb => cb([].slice.call(arguments, 1)))
   }
 
   /**
@@ -70,21 +70,20 @@ export default function Eventbus(eventsList, instance) {
    * @param {Eventbus} bus Target event bus
    * @returns {Eventbus} Target event bus
    */
-  this.sendTo = send(this.events)
+  this.sendTo = send(eventsList)
 
   /**
    * Creates a copy of Eventbus
-   * @param {Object} instance (Optional) Instance that will be returned after `.on()`
    * @returns {Eventbus} Cloed event bus
    */
-  this.fork = instance => new Eventbus(this.events, instance)
+  this.fork = () => new Eventbus(eventsList)
 
   /**
    * Merge two event buses into one
    * @param {Eventbus} bus Event bus to merge with
    * @returns {Eventbus} Event bus with both listeners
    */
-  this.merge = bus => this.fork().onMany(bus.events)
+  this.merge = bus => this.fork().onMany(eventsList)
 
   /**
    * Adds `.on` and `.emit` methods and events list to object
