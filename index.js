ca39ab0d2cface9ea3ba288132d7647ddbf69b97
Anton Kosykh <kelin2025@yandex.ru>
2018-05-09 15:42:58 +0300

    Hotfix

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ export default function Eventbus(eventsList) {
       self.off(evt, selfDesctructable)
     }
 
-    return this.on(evt, selfDesctructable)
+    return self.on(evt, selfDesctructable)
   }
 
   /**
